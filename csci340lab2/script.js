53745f801b3cc6ab31b51129844065d746a20392
Sammy Gaskins <sammycole@comcast.net>
2021-09-10 00:59:56 -0500

    Set up for multiple cats

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,35 +1,49 @@
 $(document).ready(function() {
   $('#random').click(function() {
-    getAdviceAndAddCat();
+    changeAllCats();
   });
 
-  function getAdviceAndAddCat() {
-    $.ajax({
-      dataType: "json",
-      url: "https://api.adviceslip.com/advice",
-      success: function(result) {
-        var advice = result["slip"]["advice"];
-        console.log("Advice: " + advice);
-        addCat(advice);
-      },
-      error: function(xhr, status, error) {
-        console.log(error);
-      }
-    });
+  function changeAllCats() {
+    var numCats = 6
+    for (var i = 1; i <= numCats; i++) {
+      getCatUrl(i);
+    }
+    changeNames(numCats);
   }
 
-  function addCat(caption) {
+  function getCatUrl(catIndex) {
     $.ajax({
       dataType: "json",
       url: "https://cataas.com/cat?json=true&width=400&type=sq",
       success: function(catResult) {
         var id = catResult["id"];
-        console.log(id);
-        $("#cat").attr("src", "https://cataas.com/cat/" + id + "/says/" + caption + "?width=400&type=sq");
+        //console.log(id);
+        $("#cat-" + catIndex).attr("src", "https://cataas.com/cat/" + id + "?width=400&type=sq");
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+        $("#cat-" + catIndex).attr("src", "http://placekitten.com/400/400");
+      }
+    });
+    //return catUrl;
+  }
+
+  function changeNames(numCats) {
+    $.ajax({
+      url: 'https://randomuser.me/api/?results=' + numCats,
+      dataType: 'json',
+      success: function(data) {
+        //console.log(data);
+        for (var i=0; i<numCats; i++){
+          var name = data["results"][i]["name"]["first"];
+          console.log(name);
+          $("#name-" + (i+1)).text(name);
+        }
       },
       error: function(xhr, status, error) {
         console.log(error);
       }
     });
+    //return name;
   }
 });
